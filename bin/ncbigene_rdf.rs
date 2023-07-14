cdbb6d14a9b54a533777309ada7ddcb845d31976
Hirokazu Chiba <hchiba1@gmail.com>
2023-07-14 10:28:27 +0900

    Refactor ncbigene_rdf.rs for writer

--- bin/ncbigene_rdf.rs
+++ bin/ncbigene_rdf.rs
@@ -11,19 +11,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input_file = &args[1];
     let file = File::open(input_file)?;
 
-    let mut handle = io::BufWriter::new(io::stdout());
+    let mut writer = io::BufWriter::new(io::stdout());
 
-    writeln!(handle, "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .")?;
-    writeln!(handle, "@prefix dct: <http://purl.org/dc/terms/> .")?;
-    writeln!(handle, "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .")?;
-    writeln!(handle, "@prefix ncbigene: <http://identifiers.org/ncbigene/> .")?;
-    writeln!(handle, "@prefix taxid: <http://identifiers.org/taxonomy/> .")?;
-    writeln!(handle, "@prefix hgnc: <http://identifiers.org/hgnc/> .")?;
-    writeln!(handle, "@prefix mim: <http://identifiers.org/mim/> .")?;
-    writeln!(handle, "@prefix mirbase: <http://identifiers.org/mirbase/> .")?;
-    writeln!(handle, "@prefix ensembl: <http://identifiers.org/ensembl/> .")?;
-    writeln!(handle, "@prefix insdc: <http://ddbj.nig.ac.jp/ontologies/nucleotide/> .")?;
-    writeln!(handle, "@prefix : <https://dbcls.github.io/ncbigene-rdf/ontology.ttl#> .")?;
+    writeln!(writer, "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .")?;
+    writeln!(writer, "@prefix dct: <http://purl.org/dc/terms/> .")?;
+    writeln!(writer, "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .")?;
+    writeln!(writer, "@prefix ncbigene: <http://identifiers.org/ncbigene/> .")?;
+    writeln!(writer, "@prefix taxid: <http://identifiers.org/taxonomy/> .")?;
+    writeln!(writer, "@prefix hgnc: <http://identifiers.org/hgnc/> .")?;
+    writeln!(writer, "@prefix mim: <http://identifiers.org/mim/> .")?;
+    writeln!(writer, "@prefix mirbase: <http://identifiers.org/mirbase/> .")?;
+    writeln!(writer, "@prefix ensembl: <http://identifiers.org/ensembl/> .")?;
+    writeln!(writer, "@prefix insdc: <http://ddbj.nig.ac.jp/ontologies/nucleotide/> .")?;
+    writeln!(writer, "@prefix : <https://dbcls.github.io/ncbigene-rdf/ontology.ttl#> .")?;
 
     let reader = io::BufReader::new(file);
     for line in reader.lines() {
@@ -34,60 +34,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let fields: Vec<&str> = line.split('\t').collect();
 
-        writeln!(handle)?;
-        writeln!(handle, "ncbigene:{} a insdc:Gene ;", fields[1])?;
-        writeln!(handle, "    dct:identifier {} ;", fields[1])?;
-        writeln!(handle, "    rdfs:label {} ;", quote_str(fields[2]))?;
+        writeln!(writer)?;
+        writeln!(writer, "ncbigene:{} a insdc:Gene ;", fields[1])?;
+        writeln!(writer, "    dct:identifier {} ;", fields[1])?;
+        writeln!(writer, "    rdfs:label {} ;", quote_str(fields[2]))?;
         if fields[10] != "-" {
-            writeln!(handle, "    insdc:standard_name {} ;", quote_str(fields[10]))?;
+            writeln!(writer, "    insdc:standard_name {} ;", quote_str(fields[10]))?;
         }
         if fields[3] != "-" {
-            writeln!(handle, "    insdc:locus_tag {} ;", quote_str(fields[3]))?;
+            writeln!(writer, "    insdc:locus_tag {} ;", quote_str(fields[3]))?;
         }
         if fields[4] != "-" {
-            writeln!(handle, "    insdc:gene_synonym {} ;", format_str_array(fields[4]))?;
+            writeln!(writer, "    insdc:gene_synonym {} ;", format_str_array(fields[4]))?;
         }
         if fields[8] != "-" {
-            writeln!(handle, "    dct:description {} ;", quote_str(fields[8]))?;
+            writeln!(writer, "    dct:description {} ;", quote_str(fields[8]))?;
         }
         if fields[13] != "-" {
             let others = format_str_array_exclude(fields[13], fields[8]);
             if others != "" {
-                writeln!(handle, "    dct:alternative {} ;", others)?;
+                writeln!(writer, "    dct:alternative {} ;", others)?;
             }
         }
         if fields[5] != "-" {
             let link = format_link(fields[5])?;
             if let Some(link) = link {
-                writeln!(handle, "    insdc:dblink {} ;", link)?;
+                writeln!(writer, "    insdc:dblink {} ;", link)?;
             }
         }
-        writeln!(handle, "    :typeOfGene \"{}\" ;", fields[9])?;
+        writeln!(writer, "    :typeOfGene \"{}\" ;", fields[9])?;
         if fields[12] == "O" {
-            writeln!(handle, "    :nomenclatureStatus \"official\" ;")?;
+            writeln!(writer, "    :nomenclatureStatus \"official\" ;")?;
         } else if fields[12] == "I" {
-            writeln!(handle, "    :nomenclatureStatus \"interim\" ;")?;
+            writeln!(writer, "    :nomenclatureStatus \"interim\" ;")?;
         }
         if fields[11] != "-" {
-            writeln!(handle, "    :fullName \"{}\" ;", fields[11])?;
+            writeln!(writer, "    :fullName \"{}\" ;", fields[11])?;
         }
         if fields[5] != "-" {
             let db_xref = filter_str(fields[5])?;
             if let Some(db_xref) = db_xref {
-                writeln!(handle, "    insdc:db_xref {} ;", db_xref)?;
+                writeln!(writer, "    insdc:db_xref {} ;", db_xref)?;
             }
         }
         if fields[15] != "-" {
-            writeln!(handle, "    :featureType {} ;", format_str_array(fields[15]))?;
+            writeln!(writer, "    :featureType {} ;", format_str_array(fields[15]))?;
         }
-        writeln!(handle, "    :taxid taxid:{} ;", fields[0])?;
+        writeln!(writer, "    :taxid taxid:{} ;", fields[0])?;
         if fields[6] != "-" {
-            writeln!(handle, "    insdc:chromosome \"{}\" ;", fields[6])?;
+            writeln!(writer, "    insdc:chromosome \"{}\" ;", fields[6])?;
         }
         if fields[7] != "-" {
-            writeln!(handle, "    insdc:map \"{}\" ;", fields[7])?;
+            writeln!(writer, "    insdc:map \"{}\" ;", fields[7])?;
         }
-        writeln!(handle, "    dct:modified \"{}\"^^xsd:date .", format_date(fields[14]))?;
+        writeln!(writer, "    dct:modified \"{}\"^^xsd:date .", format_date(fields[14]))?;
     }
     Ok(())
 }
